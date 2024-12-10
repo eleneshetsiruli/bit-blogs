d1f4743990c874f729efa231570c99f952056468
Elene <ellebuto@gmail.com>
2024-12-10 22:46:16 +0400

    small debug

--- src/supabase/account/index.types.ts
+++ src/supabase/account/index.types.ts
@@ -1,14 +1,3 @@
-// export type FillProfileInfoPayload = {
-//   username: string;
-//   id: string;
-//   full_name_ka: string;
-//   full_name_en: string;
-//   avatar_url: string;
-//   last_name_ka: string;
-//   last_name_en: string;
-//   telephone: string;
-// };
-
 export type FillProfileInfoPayload = {
   id: string;
   username?: string | null;
